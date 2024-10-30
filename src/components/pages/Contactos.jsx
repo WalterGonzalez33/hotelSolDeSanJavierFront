8f9e39e77bdf88e45656af2c74464274a7c8b241
tomacho <tomacho28.th@gmail.com>
2024-10-30 10:39:25 -0300

    borro una clase

--- src/components/pages/Contactos.jsx
+++ src/components/pages/Contactos.jsx
@@ -14,13 +14,7 @@ function formContactos() {
     console.log(data);
   };
 
-  const containerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#f0f0f0',
-  };
+
 
   return (
     
