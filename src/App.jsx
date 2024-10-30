efc56d82e2ac336c74d760d7d6753f82cf6f38d1
Fernando Valdivia <fernandovaldivia92@gmail.com>
2024-10-30 16:43:54 -0300

    corregi el formulario y cambie el icono de gmail

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavbarComponent from "./components/common/Navbar/NavbarComponent";
 import Index from "./components/pages/Index";
-import Footer from "./components/common/Footer/Footer";
 import QuienesSomos from "./components/pages/quienesSomos";
 import Login from "./components/Login/Login";
-Login
+
 
 
 
@@ -13,7 +11,6 @@ function App() {
   return (
     <main>
       <BrowserRouter>
-        <NavbarComponent />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/habitaciones" element={<h1>Habitaciones</h1>} />
@@ -24,7 +21,6 @@ function App() {
           <Route path="*" element={<h1>404 Not found</h1>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
-        <Footer/>
       </BrowserRouter>
     </main>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 
 import  { useState } from 'react';
-import { BsFacebook, BsGoogle } from 'react-icons/bs';
+import { BsFacebook, BsEnvelopeFill  } from 'react-icons/bs';
 import { Button, Form, Col, Row, Container } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import { Link } from "react-router-dom";
@@ -61,81 +61,85 @@ const Login = () => {
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center vh-100 bg-light rounded-4">
-      <Row>
-        <Col md={12}>
-         <div className="text-center mb-4">
-            <img src={logo} alt="Logo" className='img-fluid' />
-          </div>
-          <h2 className="text-center mb-4 letter-spacing">Iniciar Sesión</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label className='fs-5' >Email</Form.Label>
-              <Form.Control
-              className='rounded-5 border-3 '
-                type="email"
-                placeholder="Ingresa tu email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                isInvalid={!!errors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Form.Group>
+    <Container
+    className=" bg-light d-flex justify-content-center align-items-center vh-100 rounded-4  "
+    style={{ maxWidth: '500px', maxHeight: '640px', margin: 'auto' }}
+  >
+    <Row>
+      <Col md={12}>
+        <div className="text-center mb-4">
+          <img src={logo} alt="Logo" className="img-fluid" />
+        </div>
+        <h2 className="text-center mb-4 letter-spacing">Iniciar Sesión</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="fs-5">Email</Form.Label>
+            <Form.Control
+              className="rounded-5 border-3"
+              type="email"
+              placeholder="Ingresa tu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              isInvalid={!!errors.email}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword" className="mt-3">
-              <Form.Label className='fs-5'>Contraseña</Form.Label>
-              <Form.Control
-              className='rounded-5 border-3'
-                type="password"
-                placeholder="Ingresa tu contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                isInvalid={!!errors.password}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.password}
-              </Form.Control.Feedback>
-            </Form.Group>
+          <Form.Group controlId="formBasicPassword" className="mt-3">
+            <Form.Label className="fs-5">Contraseña</Form.Label>
+            <Form.Control
+              className="rounded-5 border-3"
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              isInvalid={!!errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <div className="d-flex justify-content-between mt-4">
-              <Button variant='outline-primary rounded-5' type="submit">
-                Iniciar Sesión
-              </Button>
-              <Link to="/">
+          <div className="d-flex justify-content-between mt-4">
+            <Button variant="outline-primary rounded-5" type="submit">
+              Iniciar Sesión
+            </Button>
+            <Link to="/">
               <Button variant="outline-success rounded-5" type="button">
                 Registrarse
               </Button>
-              </Link>
-            </div>
-          </Form>
-
-          <div className="text-center mt-4 ">
-  <p className='fs-5'>O registrarse con</p>
-  <div className="d-flex justify-content-center gap-3">
-    <a href="https://www.facebook.com/login" style={{ textDecoration: 'none' }}>
-      <Button variant="primary" className="d-flex align-items-center rounded-5">
-        <BsFacebook size={20} className="me-2" /> Facebook
-      </Button>
-    </a>
-    <a href="https://accounts.google.com/login"style={{ textDecoration: 'none' }}>
-      <Button variant="danger" className="d-flex align-items-center rounded-5">
-        <BsGoogle size={20} className="me-2" /> Google
-      </Button>
-    </a>
-  </div>
-</div>
-<div className="text-center mt-4">
-            <Link to="/Index.jsx">
-              <Button variant="dark rounded-5">
-                Volver al inicio
-              </Button>
             </Link>
           </div>
-        </Col>
-      </Row>
-    </Container>
+        </Form>
+
+        <div className="text-center mt-4">
+          <p className="fs-5">O registrarse con</p>
+          <div className="d-flex justify-content-center gap-3">
+            <a href="https://www.facebook.com/login" style={{ textDecoration: 'none' }}>
+              <Button variant="primary" className="d-flex align-items-center rounded-5">
+                <BsFacebook size={20} className="me-2" /> Facebook
+              </Button>
+            </a>
+            <a href="https://accounts.google.com/login" style={{ textDecoration: 'none' }}>
+              <Button variant="danger" className="d-flex align-items-center rounded-5">
+                <BsEnvelopeFill size={20} className="me-2" /> Gmail
+              </Button>
+            </a>
+          </div>
+        </div>
+
+        <div className="text-center mt-4">
+          <Link to="/Index.jsx">
+            <Button variant="dark rounded-5">
+              Volver al inicio
+            </Button>
+          </Link>
+        </div>
+      </Col>
+    </Row>
+  </Container>
   );
 };
 
