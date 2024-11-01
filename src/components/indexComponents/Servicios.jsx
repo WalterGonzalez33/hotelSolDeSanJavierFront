4534e06c0021e5203330c8478495f97d8d7eb4e5
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-10-31 21:11:07 -0300

    🎨 [STYLE] se le agrego animacion de slider a las imagenes de la seccion "servicios"

--- src/components/indexComponents/Servicios.jsx
+++ src/components/indexComponents/Servicios.jsx
@@ -1,44 +1,123 @@
-import { Container, Row, Col } from "react-bootstrap";
-
-import restaurante from "../../assets/imgInicio/restaurante2.jpg"
-import spa from "../../assets/imgInicio/spa2.jpg"
-import piscina from "../../assets/imgInicio/piscina2.jpg"
-import salaDeJuegos from "../../assets/imgInicio/sala2.jpg"
-import canchas from "../../assets/imgInicio/canchas.jpg"
-import gym from "../../assets/imgInicio/gym2.jpg"
+import { Container, Row } from "react-bootstrap";
+import restaurante from "../../assets/imgInicio/restaurante2.jpg";
+import spa from "../../assets/imgInicio/spa2.jpg";
+import piscina from "../../assets/imgInicio/piscina2.jpg";
+import salaDeJuegos from "../../assets/imgInicio/sala2.jpg";
+import canchas from "../../assets/imgInicio/canchas.jpg";
+import gym from "../../assets/imgInicio/gym2.jpg";
+import "../../styles/servicios.css";
 
 const Servicios = () => {
-    return (
-        <Container className="seccion3">
-            <Row>
-                <h2 className="colorVerdeLetra py-3 text-decoration-underline">Comodidades y Servicios</h2>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={restaurante} alt="restaurante" className=" rounded-circle w-100 imagenHotel" />
-                <h4 className="colorVerdeLetra py-2">Restaurante</h4>
-                </Col>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={spa} alt="spa" className=" rounded-circle w-100 imagenHotel"/>
-                <h4 className="colorVerdeLetra py-2">Spa</h4>
-                </Col>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={piscina} alt="piscina" className=" rounded-circle w-100 imagenHotel"/>
-                <h4 className="colorVerdeLetra py-2">Piscina</h4>
-                </Col>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={salaDeJuegos} alt="sala de juegos" className=" rounded-circle w-100 imagenHotel"/>
-                <h4 className="colorVerdeLetra py-2">Sala de Juegos</h4>
-                </Col>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={canchas} alt="spa" className=" rounded-circle w-100 imagenHotel"/>
-                <h4 className="colorVerdeLetra py-2">Canchas Deportivas</h4>
-                </Col>
-                <Col xs="6" md="4" lg className="text-center">
-                <img src={gym} alt="gym" className=" rounded-circle w-100 imagenHotel"/>
-                <h4 className="colorVerdeLetra py-2">Gym</h4>
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container className="section-services">
+      <Row>
+        <h2 className="colorVerdeLetra py-3 text-center mb-3">
+          Comodidades y Servicios
+        </h2>
+
+        <div className="slider">
+          <div className="slider-track">
+            <div className="slide">
+              <img
+                src={restaurante}
+                alt="restaurante"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Restaurante</h4>
+            </div>
+            <div className="slide">
+              <img src={spa} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Spa</h4>
+            </div>
+            <div className="slide">
+              <img src={piscina} alt="piscina" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Piscina</h4>
+            </div>
+            <div className="slide">
+              <img
+                src={salaDeJuegos}
+                alt="sala de juegos"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Sala de Juegos</h4>
+            </div>
+            <div className="slide">
+              <img src={canchas} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Canchas Deportivas</h4>
+            </div>
+            <div className="slide">
+              <img src={gym} alt="gym" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Gym</h4>
+            </div>
+            <div className="slide">
+              <img
+                src={restaurante}
+                alt="restaurante"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Restaurante</h4>
+            </div>
+            <div className="slide">
+              <img src={spa} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Spa</h4>
+            </div>
+            <div className="slide">
+              <img src={piscina} alt="piscina" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Piscina</h4>
+            </div>
+            <div className="slide">
+              <img
+                src={salaDeJuegos}
+                alt="sala de juegos"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Sala de Juegos</h4>
+            </div>
+            <div className="slide">
+              <img src={canchas} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Canchas Deportivas</h4>
+            </div>
+            <div className="slide">
+              <img src={gym} alt="gym" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Gym</h4>
+            </div>
+            <div className="slide">
+              <img
+                src={restaurante}
+                alt="restaurante"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Restaurante</h4>
+            </div>
+            <div className="slide">
+              <img src={spa} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Spa</h4>
+            </div>
+            <div className="slide">
+              <img src={piscina} alt="piscina" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Piscina</h4>
+            </div>
+            <div className="slide">
+              <img
+                src={salaDeJuegos}
+                alt="sala de juegos"
+                className=" rounded-circle"
+              />
+              <h4 className="colorVerdeLetra py-2">Sala de Juegos</h4>
+            </div>
+            <div className="slide">
+              <img src={canchas} alt="spa" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Canchas Deportivas</h4>
+            </div>
+            <div className="slide">
+              <img src={gym} alt="gym" className=" rounded-circle" />
+              <h4 className="colorVerdeLetra py-2">Gym</h4>
+            </div>
+          </div>
+        </div>
+      </Row>
+    </Container>
+  );
 };
 
 export default Servicios;
