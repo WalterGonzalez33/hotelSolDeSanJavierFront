f7fc7fc28e2b1402e814ab2b78dd01e5bd6ae0f3
tomacho <tomacho28.th@gmail.com>
2024-11-01 09:09:17 -0300

    listo estetica de form contacto

--- src/components/pages/Contactos.jsx
+++ src/components/pages/Contactos.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import "../css/index.css"
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Registro = () => {
   const {
@@ -22,7 +22,14 @@ const Registro = () => {
             className=" mb-1 letter-spacing "
             onSubmit={handleSubmit(onSubmit)}
           >
-            <h4 className="mt-4  mb-4 mx-5 letter-spacin">CONTACTO</h4>
+            <h1 className="mt-4  mb-4 mx-5 letter-spacin">CONTACTO</h1>
+            <h6 className="text-center">
+              <i class="bi bi-telephone"></i> (+54 9) (0381) 155-279796
+            </h6>
+            <h6 className="text-center">
+              <i class="bi bi-geo-alt"></i> Ruta 340 - Km 23, San Javier
+            </h6>
+            <h6 className="text-center">Tucuman, Argentina</h6>
             <Form.Group
               className="mb-3 mt-4"
               controlId="exampleForm.ControlInput1"
@@ -51,14 +58,12 @@ const Registro = () => {
               {errors.nombreyapellido?.message}
             </Form.Text>
 
-            
-
             <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
               <Form.Label>Ingrese su E-mail</Form.Label>
               <Form.Control
                 className=" rounded-5 border-3 "
                 type="email"
-                placeholder="Juan@gmail.com"
+                placeholder="Ej: Juan@gmail.com"
                 {...register("email", {
                   required: "El email es obligatorio",
                   minLength: {
@@ -73,16 +78,16 @@ const Registro = () => {
                 })}
               />
               <Form.Text className="text-danger">
-              {errors.email?.message}
-            </Form.Text>
+                {errors.email?.message}
+              </Form.Text>
             </Form.Group>
-            
+
             <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
               <Form.Label>Ingrese su Telefono</Form.Label>
               <Form.Control
                 className=" rounded-5 border-3 "
                 type="email"
-                placeholder="3813321783"
+                placeholder="Ej: 3813321783"
                 {...register("telefono", {
                   required: "El telefono es obligatorio",
                   minLength: {
@@ -91,28 +96,45 @@ const Registro = () => {
                   },
                   maxLength: {
                     value: 10,
-                    message:
-                      "El telefono debe tener como maximo 10 numeros",
+                    message: "El telefono debe tener como maximo 10 numeros",
                   },
                 })}
               />
               <Form.Text className="text-danger">
-              {errors.telefono?.message}
-            </Form.Text>
+                {errors.telefono?.message}
+              </Form.Text>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Ingrese su mensaje</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-             
-            <Button
-              className="text-center rounded-5 border-3  ms-4 "
-              variant="success"
-              type="submit"
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
             >
-              Enviar tu consulta
-            </Button>
+              <Form.Label>Ingrese su mensaje</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Ingrese su consulta"
+                {...register("consulta", {
+                  required: "ingrese su consulta",
+                  minLength: {
+                    value: 25,
+                    message: "La consulta debe tener minimo de 25 caracteres",
+                  },
+                  maxLength: {
+                    value: 300,
+                    message: "La consulta debe tener minimo de 300 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.consulta?.message}
+              </Form.Text>
+            </Form.Group>
+            <div className="d-flex justify-content-center text-center rounded-5 border-4 ">
+              <Button variant="success" type="submit">
+                Enviar
+              </Button>
+            </div>
           </Form>
         </Col>
       </Row>
