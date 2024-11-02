f396d78b170c9427536e6b9bd1c42c2c39f060a6
Mauro Escritorio <mauroconiglioo48@gmail.com>
2024-11-02 16:33:02 -0300

    fix de cards en pagina de habitaciones

---
+++ src/components/pages/Rooms.jsx
@@ -0,0 +1,26 @@
+import useFetch from "../../hooks/useFetch.jsx";
+import CardRoom from "../common/CardRoom/CardRoom.jsx";
+import { Container } from "react-bootstrap";
+
+const RoomsContainer = () => {
+    const { data, loading, error } = useFetch({ endPoint: `/rooms` });
+  
+    return (
+      <Container>
+        {data !== null &&
+          data.length > 0 &&
+          data.map((room) => (
+            <CardRoom
+              key={room.id}
+              room_name={room.room_name}
+              price={room.price}
+              image={room.image}
+              broad_description={room.broad_description}
+              benefits={room.benefits}
+            />
+          ))}
+      </Container>
+    );
+  };
+
+  export default RoomsContainer;
