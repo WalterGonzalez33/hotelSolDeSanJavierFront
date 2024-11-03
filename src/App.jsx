b522bf61ebc7885171c64598c403c2502f34844a
MauroConiglio <mauroconiglioo48@gmail.com>
2024-11-03 11:32:16 -0300

    Revert "Pagina contactos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./components/pages/Index";
 import Footer from "./components/common/Footer/Footer";
 import QuienesSomos from "./components/pages/quienesSomos";
@@ -7,22 +7,14 @@ import Login from "./components/pages/Login/Login.jsx";
 import Gallery from "./components/pages/Gallery.jsx";
 import FilterRoomsContain from "./components/FilterRoomsContainer/FilterRoomsContainer.jsx";
 import NavbarComponent from "./components/common/Navbar/NavbarComponent.jsx";
+import RoomsContainer from "./components/pages/Rooms.jsx";
 
 
 function App() {
   return (
     <main>
       <BrowserRouter>
-        <NavbarComponent />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/sobre-nosotros" element={<h1>Quines somos</h1>} />
-          <Route path="/admin" element={<h1>Administrador</h1>} />
-          <Route path="/galeria" element={<h1>galeria</h1>} />
-          <Route path="/contacto" element={<Contactos/>} />
-          <Route path="*" element={<h1>404 Not found</h1>} />
-        </Routes>
-        <Footer/>
+        <AppContent />
       </BrowserRouter>
     </main>
   );
