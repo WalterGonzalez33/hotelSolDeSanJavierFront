b9a32fbb7d7754c5041000794a271e1cf1585f75
MauroConiglio <mauroconiglioo48@gmail.com>
2024-11-03 12:34:15 -0300

    Revert "feature/Register"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./components/pages/Index";
-// import Footer from "./components/common/Footer/Footer";
+import Footer from "./components/common/Footer/Footer";
 import QuienesSomos from "./components/pages/quienesSomos";
 import ReservationForm from "./components/pages/ReservationForm";
 import Login from "./components/pages/Login/Login.jsx";
 import Gallery from "./components/pages/Gallery.jsx";
 import FilterRoomsContain from "./components/FilterRoomsContainer/FilterRoomsContainer.jsx";
 import NavbarComponent from "./components/common/Navbar/NavbarComponent.jsx";
-import Registro from "./components/pages/Registro.jsx";
+import RoomsContainer from "./components/pages/Rooms.jsx";
+import Contactos from "./components/pages/Contactos.jsx";
+
 
 function App() {
   return (
@@ -33,16 +35,16 @@ const AppContent = () => {
             element={<FilterRoomsContain />}
           />
         </Route>
-        <Route path="/habitaciones" element={<h1>Habitaciones</h1>} />
+        <Route path="/habitaciones" element={<RoomsContainer/>} />
         <Route path="/sobre-nosotros" element={<QuienesSomos />} />
         <Route path="/admin" element={<h1>Administrador</h1>} />
         <Route path="/galeria" element={<Gallery />} />
-        <Route path="/contacto" element={<h1>Contacto</h1>} />
+        <Route path="/contacto" element={<Contactos />} />
         <Route path="*" element={<h1>404 Not found</h1>} />
         <Route path="/reservacion" element={<ReservationForm />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/registro" element={<Registro />} />
       </Routes>
+      <Footer></Footer>
     </>
   );
 };
