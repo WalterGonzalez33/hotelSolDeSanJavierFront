e94fd27301bda0b1168577b28f1c5bf331c36823
G1U5 <isaiasgius@outlook.es>
2024-11-24 20:07:09 -0300

    fix validacioens del check_in y check_out

--- src/components/pages/ReservationForm.jsx
+++ src/components/pages/ReservationForm.jsx
@@ -1,13 +1,17 @@
 import { useForm } from "react-hook-form";
+import "../../styles/global.css";
 import { Button, Col, Form, Row, Carousel } from "react-bootstrap";
 import Swal from "sweetalert2";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import image1 from "../../assets/imgInicio/vista2.jpg";
 import image2 from "../../assets/imgInicio/salaDeJuegos.jpg";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
 
 const ReservationForm = () => {
   const navigate = useNavigate();
+  const [reservationData, setReservationData] = useState(null);
+
   const {
     register,
     handleSubmit,
@@ -15,7 +19,7 @@ const ReservationForm = () => {
     reset,
     watch,
   } = useForm();
-  
+
   const checkInDate = watch("checkIn");
 
   useEffect(() => {
@@ -25,10 +29,52 @@ const ReservationForm = () => {
     }
   }, [navigate]);
 
-  const reservaRealizada = async (data) => {
-    const token = sessionStorage.getItem("usuariosHotel");
-    const userId = JSON.parse(token)?.userId;
+  const token = JSON.parse(sessionStorage.getItem("usuariosHotel"));
+
+  
+  const { data, loading, error } = useFetch({
+    endPoint: "/reservations",
+    reqMethod: "POST",
+    bodyData: reservationData,
+    reload: reservationData,
+  });
+
+  const reservaRealizada = (formData) => {
+    const newReservationData = {
+      check_in: formData.check_in,
+      check_out: formData.check_out,
+      personas: formData.personas,
+      room_id: formData.room_id,
+      comentarios: formData.comentarios || "",
+      email: token?.email,
+    };
+
+  
+    setReservationData(newReservationData);
   };
+
+  useEffect(() => {
+    if (data) {
+      Swal.fire({
+        title: "Reserva Realizada",
+        text: data.reservationId
+          ? `Tu reserva fue procesada exitosamente. ID de reserva: ${data.reservationId}.`
+          : "Tu solicitud de reserva fue procesada exitosamente.",
+        icon: "success",
+        confirmButtonText: "Aceptar",
+      });
+      reset(); // Limpia el formulario
+    }
+
+    if (error) {
+      Swal.fire({
+        title: "Error",
+        text: "Hubo un problema al procesar tu reserva. Por favor, inténtalo de nuevo más tarde.",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    }
+  }, [data, error, reset]);
 
   return (
     <section className="container my-4">
@@ -60,7 +106,7 @@ const ReservationForm = () => {
               <Form.Label className="mt-2">Check-In</Form.Label>
               <Form.Control
                 type="date"
-                {...register("checkIn", {
+                {...register("check_in", {
                   required: "La fecha de Check-In es obligatoria",
                 })}
               />
@@ -69,7 +115,7 @@ const ReservationForm = () => {
               <Form.Label className="mt-4">Check-Out</Form.Label>
               <Form.Control
                 type="date"
-                {...register("checkOut", {
+                {...register("check_out", {
                   required: "La fecha de Check-Out es obligatoria",
                   validate: (value) => {
                     if (checkInDate && new Date(value) < new Date(checkInDate)) {
