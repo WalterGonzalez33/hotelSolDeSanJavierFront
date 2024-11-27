66993df201ec559beae5aae65fe86fc78849fcfe
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-11-27 12:41:19 -0300

    🎨 [STYLE] nuevos estilos para los tabs del administrador

--- src/components/pages/Admin/Admin.jsx
+++ src/components/pages/Admin/Admin.jsx
@@ -2,14 +2,29 @@ import { Container } from "react-bootstrap";
 import style from "./Admin.module.css";
 import NavAdmin from "../../adminComponents/NavAdmin/NavAdmin";
 import { Outlet, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Admin = () => {
+  const [width, setWidth] = useState(window.innerWidth);
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    const body = document.querySelector("body");
+    if (width >= 992) {
+      body.style.overflow = "hidden";
+    }
+    return () => {
+      body.style.overflow = "auto";
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [width]);
 
   return (
     <Container fluid id="main-content">
